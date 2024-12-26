8ca2fa9da15885efcf366a04d74ba6350fd4ffa1
polyarniik <57903145+polyarniik@users.noreply.github.com>
2024-12-26 13:29:14 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-pip install uv
-PORT=${PORT:-8000}
-uv run gunicorn -w 5 -b 0.0.0.0:$PORT page_analyzer:app
+curl -LsSf https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+uv sync
+uv run gunicorn -w 5 -b 0.0.0.0:$8000 page_analyzer:app
