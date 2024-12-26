4ae5d36858e6655286a943b81bc90a80e2866c66
polyarniik <57903145+polyarniik@users.noreply.github.com>
2024-12-26 13:33:19 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 curl -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 uv sync
-uv run gunicorn -w 5 -b 0.0.0.0:$8000 page_analyzer:app
+uv run gunicorn -w 5 -b 0.0.0.0:8000 page_analyzer:app
